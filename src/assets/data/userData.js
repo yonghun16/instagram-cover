18e6c3fdf49522c781f6865c3315bdcae7d3669c
yonghun16 <yonghun16@naver.com>
2025-05-20 01:09:34 +0900

    add : 더미데이타 1차 추가

--- src/assets/data/userData.js
+++ src/assets/data/userData.js
@@ -1,31 +1,118 @@
-import { avatarImageMap } from './avatarImageMap';
+/* avatars */
+import Ruffles from '../avatar/Avatar1.png';
+import sabanok from '../avatar/Avatar2.png';
+import blue_bouy from '../avatar/Avatar3.png';
+import waggles from '../avatar/Avatar4.png';
+import stveio from '../avatar/Avatar5.png';
 
-const rawUserData = [
+/* postImages */
+import Ruffles1 from '../post/Ruffles1.png';
+import Ruffles2 from '../post/Ruffles2.jpg';
+import sabanok1 from '../post/sabanok1.jpg';
+import sabanok2 from '../post/sabanok2.jpg';
+import blue_bouy1 from '../post/blue_bouy1.jpg';
+import blue_bouy2 from '../post/blue_bouy2.jpg';
+import waggles1 from '../post/waggles1.jpg';
+import waggles2 from '../post/waggles2.jpg';
+import stveio1 from '../post/stveio1.jpg';
+import stveio2 from '../post/stveio2.jpg';
+
+const userData = [
   { 
     name: 'Ruffles', 
-    newStory: false
+    image: Ruffles,
+    newStory: false,
+    stories: [
+      {
+        postImage: Ruffles1,
+        likes: 100,
+        comments: 200,
+        story: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, autem.",
+      },
+      {
+        postImage: Ruffles2,
+        likes: 40,
+        comments: 20,
+        story: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, autem.",
+      }
+    ]
   },
   { 
     name: 'sabanok',
-    newStory: true
+    image: sabanok,
+    newStory: true,
+    stories: [
+      {
+        postImage: sabanok1,
+        likes: 100,
+        comments: 200,
+        story: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, autem.",
+      },
+      {
+        postImage: sabanok2,
+        likes: 40,
+        comments: 20,
+        story: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, autem.",
+      }
+    ]
   },
   { 
     name: 'blue_bouy',
-    newStory: true
+    image: blue_bouy,
+    newStory: true,
+    stories: [
+      {
+        postImage: blue_bouy1,
+        likes: 10,
+        comments: 20,
+        story: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, autem.",
+      },
+      {
+        postImage: blue_bouy2,
+        likes: 420,
+        comments: 220,
+        story: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, autem.",
+      }
+    ]
   },
   { 
     name: 'waggles',
-    newStory: true
+    image: waggles,
+    newStory: true,
+    stories: [
+      {
+        postImage: waggles1,
+        likes: 110,
+        comments: 2,
+        story: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, autem.",
+      },
+      {
+        postImage: waggles2,
+        likes: 40,
+        comments: 5,
+        story: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, autem.",
+      }
+    ]
   },
   { 
     name: 'stve.io',
-    newStory: true
+    image: stveio,
+    newStory: true,
+    stories: [
+      {
+        postImage: stveio1,
+        likes: 1000,
+        comments: 2001,
+        story: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, autem.",
+      },
+      {
+        postImage: stveio2,
+        likes: 19,
+        comments: 209,
+        story: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, autem.",
+      }
+    ]
   },
 ];
-
-const userData = rawUserData.map(user => ({
-  ...user,                  // name : 'Ruffles' 이런식으로 대입됨.
-  image: avatarImageMap[user.name],
-}));
 
 export default userData;
