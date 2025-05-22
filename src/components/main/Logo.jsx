f7fbce90610b19896082028ce5ab7c78af3908e1
yonghun16 <yonghun16@naver.com>
2025-05-22 20:44:07 +0900

    add : 메인 로고 모달

---
+++ src/components/main/Logo.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import MenuModal from './MenuModal';
+
+/* import images */
+import logo from '../../assets/logo.png';
+import downArrowIcon from '../../assets/icons/down_arrow.png';
+
+const LogoWrapper = styled.div`
+  display: flex;
+  position: relative;
+  cursor: pointer;
+`;
+
+const LogoImg = styled.img`
+  height: 30px;
+`;
+
+const DownArrowIcon = styled.img`
+  height: 20px;
+`;
+
+function Logo() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsOpen(prev => !prev);
+  };
+
+  return (
+    <LogoWrapper onClick={toggleModal}>
+      <LogoImg src={logo} alt="logo" />
+      <DownArrowIcon src={downArrowIcon} alt="down arrow" />
+      <MenuModal isVisible={isOpen} />
+    </LogoWrapper>
+  );
+}
+
+export default Logo;
---
+++ src/components/main/MenuModal.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import styled from "styled-components";
+
+import friendsIcon from "../../assets/icons/friends.png";
+import favoritesIcon from "../../assets/icons/favorites.png";
+
+const MiniModal = styled.div`
+  position: absolute;
+  top: 40px;
+  left: 0;
+  background-color: white;
+  width: 130px;
+  box-shadow: 3px 4px 10px rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
+  z-index: 100;
+  display: ${props => (props.$visible ? 'block' : 'none')};
+`;
+
+const ModalItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 6px;
+  padding-left: 14px;
+  font-size: 16px;
+  cursor: pointer;
+  border-bottom: 1px solid #e0e0e0;
+
+  &:last-child {
+    border-bottom: none;
+  }
+
+  &:hover {
+    background-color: #f0f0f0;
+  }
+`;
+
+const Icon = styled.img`
+  height: 20px;
+  margin-right: 10px;
+`;
+
+const MenuModal = ({ isVisible }) => {
+  return (
+    <MiniModal $visible={isVisible}>
+      <ModalItem>
+        팔로잉
+        <Icon src={friendsIcon} />
+      </ModalItem>
+      <ModalItem>
+        즐겨찾기
+        <Icon src={favoritesIcon} />
+      </ModalItem>
+    </MiniModal>
+  );
+};
+
+export default MenuModal;
